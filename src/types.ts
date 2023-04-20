70f2a876ec9c5b07cd9e929c947a32497e9914fa
UZH-JingCao <jing.cao@uzh.ch>
2023-04-20 13:57:22 +0200

    close #10

---
+++ src/types.ts
@@ -0,0 +1,74 @@
+import {ReactNode} from "react";
+
+declare interface PokerProps{
+    value:number;
+    type:number;
+}
+
+declare interface GameContextProps{
+    userList?:Array<AdminProps>;
+    //地主牌
+    pokers?:Array<PokerProps>
+    //地主
+    start?:number;
+    //上一副牌
+    last?:PokerCombinationProps;
+    //当前出牌对象
+    user?:AdminProps;
+    //房间号
+    roomId:String;
+    gameStatus:number;
+
+    myUser?:AdminProps;
+    leftUser?:AdminProps;
+    rightUser?:AdminProps;
+
+    //当前出牌编号
+    now:number;
+    winner?:number;
+
+    code:string;
+}
+
+declare interface ResultProps<T> {
+    status:number;
+    data:T;
+    msg:string;
+}
+
+declare interface PokerCombinationProps{
+    userId:number;
+    card:Array<PokerProps>;
+}
+
+declare interface AdminProps{
+    id:number;
+    name:string;
+    verified:boolean;
+    token:string;
+    hasPassword?:boolean;
+
+    handCard?:Array<PokerProps>;
+    head?:string;
+    //出牌状态 on 等待 off
+    status:string;
+    isContinue?:boolean;
+}
+
+declare interface ModalFormProps{
+    fields:any;
+    currentValue?:any;
+    url:string;
+    name:string;
+    children:ReactNode;
+    variant:string;
+    allowDelete?:boolean;
+}
+
+
+declare interface StepsFromProps{
+    fields:any;
+    url:string;
+    children:ReactNode;
+    noRedirect?:boolean;
+}
