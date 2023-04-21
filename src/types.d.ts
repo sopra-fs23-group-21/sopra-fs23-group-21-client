c277f1aa5872b32b63a2575513adc7f6d4690eda
UZH-JingCao <91093682+UZH-JingCao@users.noreply.github.com>
2023-04-21 21:59:15 +0200

    Fix types issue, fix#26

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,80 +1,79 @@
-//import {ReactNode} from "react";
 
-//properties of cards
 declare interface PokerProps{
-    value:number;
-    type:number;
+  value: number,
+  type: number
 }
 
-//properties of gamecontext
 declare interface GameContextProps{
-    userList?:Array<AdminProps>;
-    //地主牌
-    pokers?:Array<PokerProps>
-    //地主
-    start?:number;
-    //上一副牌
-    last?:PokerCombinationProps;
-    //当前出牌对象
-    user?:AdminProps;
-    //房间号
-    roomId:String;
-    gameStatus:number;
+ //用户
+ userList?:Array<AdminProps>;
+ //地主牌
+ pokers?:Array<PokerProps>;
+ //地主
+ start?: number;
+ //上一副牌
+ last?:PokerCombinationProps;
+ //当前出牌对象
+ user?:AdminProps;
+ //房间号
+ roomId:string;
+ gameStatus:number;
 
-    myUser?:AdminProps;
-    leftUser?:AdminProps;
-    rightUser?:AdminProps;
+ myUser?:AdminProps;
+ leftUser?:AdminProps;
+ rightUser?:AdminProps;
 
-    //当前出牌编号
-    now:number;
-    winner?:number;
+ //当前出牌编号
+ now:number;
+ winner?:number;
 
-    code:string;
 }
 
-//properties of result
 declare interface ResultProps<T> {
-    status:number;
+    status: number;
+
     data:T;
+
     msg:string;
+
 }
 
-//properties of card combination
-declare interface PokerCombinationProps{
-    userId:number;
-    card:Array<PokerProps>;
+declare interface PokerCombinationProps {
+  userId: number;
+
+  card:Array<PokerProps>;
+
+
 }
 
+declare interface AdminProps {
+  id: number;
+  name: string;
+  verified: boolean;
+  token: string;
+  hasPassword?: boolean;
 
-//
-declare interface AdminProps{
-    id:number;
-    name:string;
-    verified:boolean;
-    token:string;
-    hasPassword?:boolean;
+  handCard?:Array<PokerProps>;
 
-    handCard?:Array<PokerProps>;
-    head?:string;
-    //出牌状态 on 等待 off
-    status:string;
-    isContinue?:boolean;
+  head?:string;
+  //出牌状态 on 等待阶段 off
+  status:string;
+  isContinue?:boolean;
 }
 
-declare interface ModalFormProps{
-    fields:any;
-    currentValue?:any;
-    url:string;
-    name:string;
-    children:ReactNode;
-    variant:string;
-    allowDelete?:boolean;
+declare interface ModalFormProps {
+  fields: any;
+  currentValues?: any;
+  url: string;
+  name: string;
+  children: ReactNode;
+  variant: string;
+  allowDelete?: boolean;
 }
 
-
-declare interface StepsFormProps{
-    fields:any;
-    url:string;
-    children:ReactNode;
-    noRedirect?:boolean;
+declare interface StepsFormProps {
+  fields: any;
+  url: string;
+  children: ReactNode;
+  noRedirect?: boolean;
 }
