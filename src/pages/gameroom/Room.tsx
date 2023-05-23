cd47446a33c0cf9545800ca25c9f63a4f4daa8e7
zhi wang <zhi.wang@uzh.ch>
2023-05-23 18:38:42 +0200

    create game ui rules

--- src/pages/gameroom/Room.tsx
+++ src/pages/gameroom/Room.tsx
@@ -1,9 +1,9 @@
 import {
-    Box,
-    Center,
-    Container,
-    Flex, Image,
-    Text
+  Box, Button,
+  Center,
+  Container,
+  Flex, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay,
+  Text
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -22,6 +22,14 @@ export default function Room() {
   const { roomId } = useParams();
   //音乐地址
   const audioUrl = "/music/667.mp3";
+  //弹窗
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
 
   //上一副牌
   const [last, setLast] = useState<Array<PokerProps> | undefined>(undefined);
@@ -204,6 +212,25 @@ export default function Room() {
             </Box>
           ))}
         </Container>
+        {/* 加弹窗按钮 */}
+        <Flex
+            justifyContent="flex-end"
+            position="absolute" // 使用绝对定位
+            zIndex={1} // 设置按钮的层级
+            top={5}
+            right={100}
+        >
+          <Button onClick={handleOpen}
+                  bg="white"
+                  color="blue"
+                  opacity={0.7}
+                  fontWeight={700}
+                  fontSize={20}
+                  _hover={{ color: "white", bgColor:"#4b424a"}}
+                  _active={{ color: "white", bgColor:"#4b424a" }}
+          >Game Rule</Button> {/* 设置按钮的背景颜色为透明 */}
+        </Flex>
+
       </Box>
       <Flex direction="row" flex="1">
         {/* Left */}
@@ -266,6 +293,40 @@ export default function Room() {
         />
         <MusicPlayer audioUrl={audioUrl} />
       </Box>
+      <Modal isOpen={isOpen} onClose={handleClose}>
+        <ModalContent
+            maxWidth="1000px"
+            bg="rgba(0, 0, 0, 0.8)" // 设置背景颜色和透明度
+            color="white" // 设置字体颜色为白色
+        >
+          <ModalHeader>Game Role</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <p>1. Our poker game is a three-player game. To start the game, all three players must be online at the same time, enter the same game room, and click ' 准备 ready？'，the '准备 Ready?' button is unavailable until there are three players in the same game room. If only one or two players are present, or if all three players are not online at the same time, the game cannot be played.
+            </p>
+            <p>2. After finishing a game, it is best for all three players to either choose to play another round together or all choose to exit the room, instead of one player continuing while the other two choose to exit.
+            </p>
+            <p>3. The rules of the game "Doudizhu" are relatively complicated: In short, this is a team-based game, with two farmers as one team and the landlord as the other team. Whoever plays all their cards first wins, and as long as one of the farmers plays all the cards first, the farmers win.
+            </p>
+            <p>4. At the beginning of the game, the three players can compete to become the landlord. Each player is randomly dealt 17 cards, and an additional three cards are given to the player who successfully becomes the landlord. Therefore, each farmer has 17 cards, while the landlord has 20 cards and can play first.
+            </p>
+            <p>5. The types of cards include single cards (A), double cards (AA), three cards with one (AAA+6), three cards with two (AAA+66), four cards (AAAA), consecutive cards (4-5-6-7-8), consecutive double cards (44-55-66), consecutive three cards (444-555), consecutive three cards with one (444-555-7-9), and the pair of jokers (red joker + black joker).
+            </p>
+            <p>6. In "Doudizhu", the card values are as follows: 3 &lt; 4 &lt; 5 &lt; 6 &lt; 7 &lt; 8 &lt; 9 &lt; 10 &lt; J &lt; Q &lt; K &lt; A &lt; 2 &lt; RED JOKER &lt; BLACK JOKER. The size relationship between card combinations is as follows: single cards(A), double cards(AA), three cards(AAA), three cards with one(222+6), three cards with two(JJJ+K), consecutive single cards(10-J-Q-K-A, note:at least 5 cards), consecutive double cards(JJ-QQ-KK,at least 3 cards combination), consecutive three cards(888-999, at least 2 card combination), consecutive three cards with one(QQQ-KKK-2-5) are all at the same level. This means that single cards can only be played against single cards, double cards can only be played against double cards, and other card combinations can only be compared within their respective card combinations. For example, if the landlord plays a 3, the farmers can play a 6, but not a 66. If the landlord plays 3-4-5-6-7, the farmers can play 4-5-6-7-8, but not any other card combinations. Four cards is a kind is a special card combination that was not mentioned earlier, as it is higher than any other card combination. Four cards can be played against any other card combination. For example, 3333 is the lowest value of four cards, but it can beat 4-5-6-7-8 and KKK-AAA-4-7. The most special card combination is the pair of jokers. The red joker and black joker can beat any card combination, including the highest four cards, which is 2222.
+              </p>
+            <p>7. For more details on "Landlord," you can search for it on Wikipedia at https://en.wikipedia.org/wiki/Dou_dizhu.
+            </p>
+            <p>8. Finally, when the game finishes, everyone need to click on "Quit Room" - This is very important!!!</p>
+
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleClose}>
+              关闭
+            </Button>
+
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
